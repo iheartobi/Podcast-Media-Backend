f53588acc448f9d83b787ba67ba34fc7327372cd
iheartobi <iobidah9@gmail.com>
2019-08-26 21:57:59 -0400

    more controller mods

--- Pod-Media-Back/app/controllers/application_controller.rb
+++ Pod-Media-Back/app/controllers/application_controller.rb
@@ -2,19 +2,19 @@ class ApplicationController < ActionController::API
     before_action :authorized
    
     def encode_token(payload)
-      # should store secret in env variable
+      
       JWT.encode(payload, 'kjblover88')
     end
    
     def auth_header
-      # { Authorization: 'Bearer <token>' }
+      
       request.headers['Authorization']
     end
    
     def decoded_token
       if auth_header
         token = auth_header.split(' ')[1]
-        # header: { 'Authorization': 'Bearer <token>' }
+        
         begin
           JWT.decode(token, 'kjblover88', true, algorithm: 'HS256')
         rescue JWT::DecodeError
